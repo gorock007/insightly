f54a1390a0cf30f793714badae6b3ca4d5dc61dc
Gorock <gorock397@gmail.com>
2024-12-04 08:47:28 +1100

    add hero & header section

--- app/page.js
+++ app/page.js
@@ -1,13 +1,35 @@
-import Image from "next/image";
+import ButtonLogin from "@/components/ButtonLogin";
 
-export default function Home() {
+const Home = () => {
+  const isLoggedIn = true;
+  let name = 'Marc';
+
   return (
     <main>
-      <h1>Collect customer feedback to build better products</h1>
-      <div>
-        Create a feedback board in minutes, prioritize features, and 
-        build products your customes will love.
-      </div>
+      <section className="bg-base-200">
+        <div className="flex justify-between items-center px-8 py-2 max-w-3xl mx-auto">
+          <div className="font-bold">Insightly</div>
+          <div className="space-x-4 max-md:hidden">
+            <a className="link link-hover">Pricing</a>
+            <a className="link link-hover">FAQ</a>
+          </div>
+          <div>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          </div>
+        </div>
+      </section>
+
+      <section className="text-center py-32 px-8 max-w-3xl mx-auto">
+        <h1 className="text-4xl font-extrabold mb-6 lg:text-5xl">
+          Collect customer feedback to build better products</h1>
+        <div className="opacity-90 mb-10"> 
+          Create a feedback board in minutes, prioritize features, and 
+          build products your customes will love.
+        </div>
+        <ButtonLogin isLoggedIn={isLoggedIn} name={name}/>
+      </section>
     </main>
   );
 }
+
+export default Home;
